3c2dc066076ddee7dc93ff30d025dfc789b979e2
abdul <abdul.tanveer@gmail.com>
2020-02-15 13:01:51 +0530

    broadcast receiver sms

---
+++ app/src/main/java/com/spot/gmailz/SmsReceiver.java
@@ -0,0 +1,16 @@
+package com.spot.gmailz;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "hey abdul", Toast.LENGTH_SHORT).show();
+    }
+}
